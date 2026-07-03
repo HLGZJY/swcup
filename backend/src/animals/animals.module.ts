4772efe46082bd6c11750ffc02ae29e3b178f062
HLGZJY <heguojun_0419@qq.com>
2026-07-03 21:06:57 +0800

    fix(animals): 注册 RescueEvent 到 AnimalsModule 的 forFeature
    修复 Bug5 引入的 DI 错误:AnimalsService 注入 @InjectRepository(RescueEvent)
    但 AnimalsModule 的 TypeOrmModule.forFeature 没把 RescueEvent 加进去,
    导致 backend 启动时 Nest 报 UnknownDependenciesException。
    
    修改:把 RescueEvent 加进 forFeature 数组。

--- backend/src/animals/animals.module.ts
+++ backend/src/animals/animals.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimalsController } from './animals.controller';
 import { AnimalsService } from './animals.service';
 import { Animal } from './entities/animal.entity';
+import { NoseFeature } from '../nose/entities/nose-feature.entity';
+import { RescueEvent } from '../events/entities/event.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Animal])],
+  imports: [TypeOrmModule.forFeature([Animal, NoseFeature, RescueEvent])],
   controllers: [AnimalsController],
   providers: [AnimalsService],
   exports: [AnimalsService],
